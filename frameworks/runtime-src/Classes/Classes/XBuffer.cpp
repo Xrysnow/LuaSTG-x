31e99e6c89bd411e88881582109e8af5b353e2c3
Xrysnow <xrysnow@outlook.com>
2019-05-14 23:22:49 +0800

    add class Buffer

---
+++ frameworks/runtime-src/Classes/Classes/XBuffer.cpp
@@ -0,0 +1,305 @@
+﻿#include "XBuffer.h"
+
+std::vector<uint8_t> vec;
+using namespace lstg;
+
+Buffer::Buffer()
+{
+}
+
+Buffer::~Buffer()
+{
+}
+
+Buffer* Buffer::create(size_t size)
+{
+	auto ret = new (std::nothrow) Buffer();
+	if (ret&&ret->init(size))
+	{
+		ret->autorelease();
+		return ret;
+	}
+	CC_SAFE_DELETE(ret);
+	return nullptr;
+}
+
+Buffer* Buffer::createFromString(const std::string& s)
+{
+	auto ret = new (std::nothrow) Buffer();
+	if (ret&&ret->init(0))
+	{
+		ret->autorelease();
+		ret->buffer.assign(s.begin(), s.end());
+		return ret;
+	}
+	CC_SAFE_DELETE(ret);
+	return nullptr;
+}
+
+Buffer* Buffer::clone() const
+{
+	auto ret = create();
+	if (!ret)
+		return nullptr;
+	ret->buffer = buffer;
+	return ret;
+}
+
+void Buffer::setWritable(bool b)
+{
+	canWrite = b;
+}
+
+void Buffer::setAppendable(bool b)
+{
+	canAppend = b;
+}
+
+void Buffer::setShrinkable(bool b)
+{
+	canShrink = b;
+}
+
+bool Buffer::isWritable() const
+{
+	return canWrite;
+}
+
+bool Buffer::isAppendable() const
+{
+	return canAppend;
+}
+
+bool Buffer::isShrinkable() const
+{
+	return canShrink;
+}
+
+bool Buffer::isResizable() const
+{
+	return canAppend && canShrink;
+}
+
+std::string Buffer::to_string() const
+{
+	return std::string((char*)buffer.data(), buffer.size());
+}
+
+uint8_t* Buffer::data() const
+{
+	return buffer.empty() ? nullptr: (uint8_t*)buffer.data();
+}
+
+void Buffer::copy(Buffer* other)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	if (other && other != this)
+		buffer = other->buffer;
+}
+
+void Buffer::move(Buffer* other)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	if (other && other != this)
+	{
+		clear();
+		std::swap(buffer, other->buffer);
+	}
+}
+
+void Buffer::assign(Buffer* other, size_t first, size_t last)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	if (!other || !other->check_pos(first) || !other->check_pos(last))
+		return;
+	if (first <= last)
+		return;
+	const auto beg = other->buffer.begin();
+	buffer.assign(beg + first, beg + last);
+}
+
+void Buffer::assign_value(size_t count, uint8_t value)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	buffer.assign(count, value);
+}
+
+void Buffer::assign_values(const std::vector<uint8_t>& values)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	buffer = values;
+}
+
+uint8_t Buffer::at(size_t index) const
+{
+	if (!check_index(index))
+		return 0;
+	return buffer[index];
+}
+
+void Buffer::set(size_t index, uint8_t value)
+{
+	if (!isWritable())
+		return;
+	if (!check_index(index))
+		return;
+	buffer[index] = value;
+}
+
+void Buffer::set_values(size_t pos, const std::vector<uint8_t>& values)
+{
+	if (!isWritable())
+		return;
+	if (!check_pos(pos))
+		return;
+	auto end = pos + values.size();
+	if (end > size())
+	{
+		if (canAppend)
+			resize(end);
+		else
+			end = size();
+	}
+	for (auto i = pos; i < end; ++i)
+	{
+		buffer[i] = values[i - pos];
+	}
+}
+
+uint8_t Buffer::front() const
+{
+	return buffer.front();
+}
+
+uint8_t Buffer::back() const
+{
+	return buffer.back();
+}
+
+bool Buffer::empty() const
+{
+	return buffer.empty();
+}
+
+size_t Buffer::size() const
+{
+	return buffer.size();
+}
+
+void Buffer::reserve(size_t size)
+{
+	if (size > buffer.size() && !canAppend)
+		return;
+	buffer.reserve(size);
+}
+
+size_t Buffer::capacity() const
+{
+	return buffer.capacity();
+}
+
+void Buffer::shrink_to_fit()
+{
+	buffer.shrink_to_fit();
+}
+
+void Buffer::clear()
+{
+	if (!isWritable() || !canShrink)
+		return;
+	buffer.clear();
+}
+
+void Buffer::insert(size_t pos, Buffer* other, size_t first, size_t last)
+{
+	if (!isWritable() || !canAppend)
+		return;
+	if (!other || !other->check_pos(first) || !other->check_pos(last))
+		return;
+	if (!check_pos(pos) || first <= last)
+		return;
+	const auto beg = other->buffer.begin();
+	buffer.insert(buffer.begin() + pos, beg + first, beg + last);
+}
+
+void Buffer::insert_value(size_t pos, size_t count, uint8_t value)
+{
+	if (!isWritable() || !canAppend)
+		return;
+	if (!check_pos(pos) || count == 0)
+		return;
+	buffer.insert(buffer.begin() + pos, count, value);
+}
+
+void Buffer::insert_values(size_t pos, const std::vector<uint8_t>& values)
+{
+	if (!isWritable() || !canAppend)
+		return;
+	if (!check_pos(pos) || values.empty())
+		return;
+	buffer.insert(buffer.begin() + pos, values.begin(), values.end());
+}
+
+void Buffer::erase(size_t first, size_t last)
+{
+	if (!isWritable() || !canShrink)
+		return;
+	if (first <= last)
+		return;
+	const auto beg = buffer.begin();
+	buffer.erase(beg + first, beg + last);
+}
+
+void Buffer::push_back(uint8_t value)
+{
+	if (!canAppend)
+		return;
+	buffer.push_back(value);
+}
+
+void Buffer::pop_back()
+{
+	if (!canShrink)
+		return;
+	if (!buffer.empty())
+		buffer.pop_back();
+}
+
+void Buffer::resize(size_t size, uint8_t value)
+{
+	if (size > buffer.size() && !canAppend)
+		return;
+	if (size < buffer.size() && !canShrink)
+		return;
+	if (value == 0)
+		buffer.resize(size);
+	else
+		buffer.resize(size, value);
+}
+
+void Buffer::swap(Buffer* other)
+{
+	if (!isWritable() || !isResizable())
+		return;
+	if (!other)
+		return;
+	buffer.swap(other->buffer);
+}
+
+bool Buffer::init(size_t size)
+{
+	try
+	{
+		buffer.resize(size);
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
+}
---
+++ frameworks/runtime-src/Classes/Classes/XBuffer.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include "cocos2d.h"
+
+namespace lstg
+{
+	class Buffer : public cocos2d::Ref
+	{
+		Buffer();
+		virtual ~Buffer();
+
+		std::vector<uint8_t> buffer;
+		bool canWrite = true;
+		bool canAppend = true;
+		bool canShrink = true;
+	public:
+		static Buffer* create(size_t size = 0);
+		static Buffer* createFromString(const std::string& s);
+		Buffer* clone() const;
+
+		void setWritable(bool b);
+		void setAppendable(bool b);
+		void setShrinkable(bool b);
+
+		bool isWritable() const;
+		bool isAppendable() const;
+		bool isShrinkable() const;
+		bool isResizable() const;
+
+		std::string to_string() const;
+		uint8_t* data() const;
+
+		void copy(Buffer* other);
+		void move(Buffer* other);
+
+		void assign(Buffer* other, size_t first, size_t last);
+		void assign_value(size_t count, uint8_t value);
+		void assign_values(const std::vector<uint8_t>& values);
+
+		uint8_t at(size_t index) const;
+		void set(size_t index, uint8_t value);
+		void set_values(size_t pos, const std::vector<uint8_t>& values);
+
+		uint8_t front() const;
+		uint8_t back() const;
+
+		bool empty() const;
+		size_t size() const;
+
+		void reserve(size_t size);
+		size_t capacity() const;
+		void shrink_to_fit();
+
+		void clear();
+		void insert(size_t pos, Buffer* other, size_t first, size_t last);
+		void insert_value(size_t pos, size_t count, uint8_t value);
+		void insert_values(size_t pos, const std::vector<uint8_t>& values);
+		void erase(size_t first, size_t last);
+		void push_back(uint8_t value);
+		void pop_back();
+		void resize(size_t size, uint8_t value = 0);
+		void swap(Buffer* other);
+
+	protected:
+		bool init(size_t size);
+		bool check_index(size_t index) const { return index < buffer.size(); }
+		bool check_pos(size_t index) const { return index <= buffer.size(); }
+	};	
+}
+
+namespace cocos2d
+{
+	template<>
+	class ResizableBufferAdapter<lstg::Buffer> : public ResizableBuffer {
+		typedef lstg::Buffer BufferType;
+		BufferType* _buffer;
+	public:
+		explicit ResizableBufferAdapter(BufferType* buffer) : _buffer(buffer) {}
+		virtual void resize(size_t size) override {
+			_buffer->resize(size);
+		}
+		virtual void* buffer() const override {
+			if (_buffer->empty())
+				return nullptr;
+			return _buffer->data();
+		}
+	};
+
+}
