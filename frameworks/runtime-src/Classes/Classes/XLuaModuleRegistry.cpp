6d09334c21dfa04943a121151bb75726f9c2dc24
Xrysnow <xrysnow@outlook.com>
2019-10-20 02:51:20 +0800

    add XLuaModuleRegistry

---
+++ frameworks/runtime-src/Classes/Classes/XLuaModuleRegistry.cpp
@@ -0,0 +1,82 @@
+﻿#include "XLuaModuleRegistry.h"
+#include "scripting/lua-bindings/manual/CCLuaEngine.h"
+
+LuaModuleRegistry* LuaModuleRegistry::get()
+{
+	static LuaModuleRegistry inst;
+	return &inst;
+}
+
+LuaModuleRegistry& LuaModuleRegistry::addModule(const std::string& name, lua_CFunction regFunc, int32_t priority)
+{
+	auto inst = get();
+	inst->modules[name] = { regFunc,priority };
+	return *inst;
+}
+
+LuaModuleRegistry& LuaModuleRegistry::listModules(std::vector<std::pair<std::string, lua_CFunction>>& out)
+{
+	auto inst = get();
+	std::multimap<int32_t, std::pair<std::string, lua_CFunction>> entries;
+	for (auto& it : inst->modules)
+		entries.insert({ it.second.second, {it.first, it.second.first} });
+	for (auto& it : entries)
+		out.push_back(it.second);
+	return *inst;
+}
+
+LuaModuleRegistry& LuaModuleRegistry::registerModules(lua_State* L)
+{
+	std::vector<std::pair<std::string, lua_CFunction>> modules;
+	listModules(modules);
+	auto top = lua_gettop(L);
+	for (auto& it : modules)
+	{
+		it.second(L);
+		const auto new_top = lua_gettop(L);
+		if (top != new_top)
+		{
+			cocos2d::log("Lua stack changed %d when register %s",
+				new_top - top, it.first.c_str());
+			top = new_top;
+		}
+	}
+	return *get();
+}
+
+LuaModuleRegistry& LuaModuleRegistry::addFunction(
+	const std::string& nameSpace, const std::string& funcName, lua_CFunction regFunc)
+{
+	auto inst = get();
+	auto& functions = inst->functions;
+	if (functions.find(nameSpace) == functions.end())
+		functions[nameSpace] = {};
+	functions[nameSpace][funcName] = regFunc;
+	return *inst;
+}
+
+LuaModuleRegistry& LuaModuleRegistry::listFunctions(
+	std::vector<std::tuple<std::string, std::string, lua_CFunction>>& out)
+{
+	auto inst = get();
+	for (auto& parent : inst->functions)
+		for (auto& it : parent.second)
+			out.emplace_back(parent.first, it.first, it.second);
+	return *inst;
+}
+
+LuaModuleRegistry& LuaModuleRegistry::registerFunctions(lua_State* L)
+{
+	auto inst = get();
+	for (auto& parent : inst->functions)
+	{
+		const auto pName = parent.first.c_str();
+		tolua_open(L);
+		tolua_module(L, pName, 0);
+		tolua_beginmodule(L, pName);
+		for (auto& it : parent.second)
+			tolua_function(L, it.first.c_str(), it.second);
+		tolua_endmodule(L);
+	}
+	return *inst;
+}
---
+++ frameworks/runtime-src/Classes/Classes/XLuaModuleRegistry.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "scripting/lua-bindings/manual/CCLuaEngine.h"
+
+class LuaModuleRegistry
+{
+	LuaModuleRegistry() = default;
+	~LuaModuleRegistry() = default;
+	std::unordered_map<std::string, std::pair<lua_CFunction, int32_t>> modules;
+	std::unordered_map<std::string, std::unordered_map<std::string, lua_CFunction>> functions;
+public:
+	LuaModuleRegistry(const LuaModuleRegistry &) = delete;
+	LuaModuleRegistry &operator =(const LuaModuleRegistry &) = delete;
+
+	static LuaModuleRegistry* get();
+
+	static LuaModuleRegistry& addModule(
+		const std::string& name,
+		lua_CFunction regFunc,
+		int32_t priority = 0);
+	static LuaModuleRegistry& listModules(
+		std::vector<std::pair<std::string, lua_CFunction>>& out);
+	static LuaModuleRegistry& registerModules(lua_State* L);
+
+	static LuaModuleRegistry& addFunction(
+		const std::string& nameSpace,
+		const std::string& funcName,
+		lua_CFunction regFunc);
+	static LuaModuleRegistry& listFunctions(
+		std::vector<std::tuple<std::string, std::string, lua_CFunction>>& out);
+	static LuaModuleRegistry& registerFunctions(lua_State* L);
+};
+
+#define LUA_REGISTER_MODULE(Name, Entry)\
+	static CC_UNUSED auto& __make_ ## Name ## _ ## Entry ## __ = \
+		LuaModuleRegistry::addModule(#Name, Entry)
+
+#define LUA_REGISTER_MODULE_DEF(Name)\
+	int LuaModule_##Name(lua_State* L);\
+	LUA_REGISTER_MODULE(Name, LuaModule_##Name);\
+	int LuaModule_##Name(lua_State* L)
+
+#define LUA_REGISTER_FUNC(ParentName, Name, Entry)\
+	static CC_UNUSED auto& __make_ ## ParentName ## _ ## Name ## _ ## Entry ## __ = \
+		LuaModuleRegistry::addFunction(#ParentName, #Name, Entry)
+
+#define LUA_REGISTER_FUNC_DEF(ParentName, Name)\
+	int LuaFunc_##ParentName##_##Name(lua_State* L);\
+	LUA_REGISTER_FUNC(ParentName, Name, LuaFunc_##ParentName##_##Name);\
+	int LuaFunc_##ParentName##_##Name(lua_State* L)
