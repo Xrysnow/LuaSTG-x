14ac3a065e5809cc946dff3a2e4aab734aff53b3
Xrysnow <xrysnow@outlook.com>
2020-03-09 00:46:00 +0800

    Update lua_cc_fix.cpp

--- frameworks/runtime-src/Classes/LuaBindings/lua_cc_fix.cpp
+++ frameworks/runtime-src/Classes/LuaBindings/lua_cc_fix.cpp
@@ -131,6 +131,85 @@ int lua_cocos2dx_EventDispatcher_addCustomEventListener00(lua_State* tolua_S)
 				return 0;
 }
 
+extern int lua_cocos2dx_GLViewImpl_createWithRect(lua_State* tolua_S);
+extern int lua_cocos2dx_GLViewImpl_create(lua_State* tolua_S);
+int lua_cocos2dx_GLViewImpl_createWithRect00(lua_State* tolua_S)
+{
+	int argc = 0;
+	bool ok = true;
+
+#if COCOS2D_DEBUG >= 1
+	tolua_Error tolua_err;
+	if (!tolua_isusertable(tolua_S, 1, "cc.GLViewImpl", 0, &tolua_err)) goto tolua_lerror;
+#endif
+
+#ifdef CC_PLATFORM_PC
+	argc = lua_gettop(tolua_S) - 1;
+
+	if (argc == 4)
+	{
+		std::string arg0;
+		cocos2d::Rect arg1;
+		double arg2;
+		bool arg3;
+		ok &= luaval_to_std_string(tolua_S, 2, &arg0, "cc.GLViewImpl:createWithRect");
+		ok &= luaval_to_rect(tolua_S, 3, &arg1, "cc.GLViewImpl:createWithRect");
+		ok &= luaval_to_number(tolua_S, 4, &arg2, "cc.GLViewImpl:createWithRect");
+		ok &= luaval_to_boolean(tolua_S, 5, &arg3, "cc.GLViewImpl:createWithRect");
+		if (!ok)
+		{
+			tolua_error(tolua_S, "invalid arguments in function 'lua_cocos2dx_GLViewImpl_createWithRect'", nullptr);
+			return 0;
+		}
+		cocos2d::GLViewImpl* ret = cocos2d::GLViewImpl::createWithRect(arg0, arg1, arg2, arg3);
+		object_to_luaval<cocos2d::GLViewImpl>(tolua_S, "cc.GLViewImpl", (cocos2d::GLViewImpl*)ret);
+		return 1;
+	}
+#endif
+	return lua_cocos2dx_GLViewImpl_createWithRect(tolua_S);
+#if COCOS2D_DEBUG >= 1
+	tolua_lerror:
+				tolua_error(tolua_S, "#ferror in function 'lua_cocos2dx_GLViewImpl_createWithRect'.", &tolua_err);
+#endif
+				return 0;
+}
+int lua_cocos2dx_GLViewImpl_create00(lua_State* tolua_S)
+{
+	int argc = 0;
+	bool ok = true;
+
+#if COCOS2D_DEBUG >= 1
+	tolua_Error tolua_err;
+	if (!tolua_isusertable(tolua_S, 1, "cc.GLViewImpl", 0, &tolua_err)) goto tolua_lerror;
+#endif
+
+#ifdef CC_PLATFORM_PC
+	argc = lua_gettop(tolua_S) - 1;
+
+	if (argc == 2)
+	{
+		std::string arg0;
+		bool arg1;
+		ok &= luaval_to_std_string(tolua_S, 2, &arg0, "cc.GLViewImpl:create");
+		ok &= luaval_to_boolean(tolua_S, 3, &arg1, "cc.GLViewImpl:create");
+		if (!ok)
+		{
+			tolua_error(tolua_S, "invalid arguments in function 'lua_cocos2dx_GLViewImpl_create'", nullptr);
+			return 0;
+		}
+		cocos2d::GLViewImpl* ret = cocos2d::GLViewImpl::create(arg0, arg1);
+		object_to_luaval<cocos2d::GLViewImpl>(tolua_S, "cc.GLViewImpl", (cocos2d::GLViewImpl*)ret);
+		return 1;
+	}
+#endif
+	return lua_cocos2dx_GLViewImpl_create(tolua_S);
+#if COCOS2D_DEBUG >= 1
+	tolua_lerror:
+				tolua_error(tolua_S, "#ferror in function 'lua_cocos2dx_GLViewImpl_createWithRect'.", &tolua_err);
+#endif
+				return 0;
+}
+
 int register_all_cocos2dx_fix(lua_State* L)
 {
 	if (nullptr == L)
@@ -149,6 +228,15 @@ int register_all_cocos2dx_fix(lua_State* L)
 	if (lua_istable(L, -1))
 	{
 		tolua_function(L, "addCustomEventListener", lua_cocos2dx_EventDispatcher_addCustomEventListener00);
+	}
+	lua_pop(L, 1);
+
+	lua_pushstring(L, "cc.GLViewImpl");
+	lua_rawget(L, LUA_REGISTRYINDEX);
+	if (lua_istable(L, -1))
+	{
+		tolua_function(L, "createWithRect", lua_cocos2dx_GLViewImpl_createWithRect00);
+		tolua_function(L, "create", lua_cocos2dx_GLViewImpl_create00);
 	}
 	lua_pop(L, 1);
 
