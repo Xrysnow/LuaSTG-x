b729119003961f08ac3861d4b859dcc10a7371bb
Xrysnow <xrysnow@outlook.com>
2020-03-11 13:08:44 +0800

    Update main.cpp

--- frameworks/runtime-src/proj.android/app/jni/hellolua/main.cpp
+++ frameworks/runtime-src/proj.android/app/jni/hellolua/main.cpp
@@ -22,26 +22,21 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#include <memory>
-
 #include <android/log.h>
 #include <jni.h>
-
-// #include "AppDelegate.h"
 #include "AppFrame.h"
+#include <memory>
 
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 
 namespace {
-// std::unique_ptr<AppDelegate> appDelegate;
-std::unique_ptr<lstg::AppFrame> appDelegate;
+    std::unique_ptr<lstg::AppFrame> appDelegate;
 }
 
 void cocos_android_app_init(JNIEnv* env) {
     LOGD("cocos_android_app_init");
-    // appDelegate.reset(new AppDelegate());
-    auto ins=lstg::AppFrame::getInstance();
+    auto ins = lstg::AppFrame::getInstance();
     appDelegate.reset(ins);
     LOGD("cocos_android_app_init finished.");
 }
