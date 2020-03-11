3d07445414ee0b6184ea54c3e4c716e09a1d0595
Xrysnow <xrysnow@outlook.com>
2020-03-11 13:47:56 +0800

    Update LWBuiltInFunction.cpp

--- frameworks/runtime-src/Classes/LSTG/LWBuiltInFunction.cpp
+++ frameworks/runtime-src/Classes/LSTG/LWBuiltInFunction.cpp
@@ -4,19 +4,7 @@
 
 using namespace std;
 using namespace lstg;
-using cocos2d::Color4B;
 
-// Misc
-LUA_REGISTER_FUNC_DEF(lstg, Snapshot)
-{
-	LAPP.snapShot(luaL_checkstring(L, 1));
-	return 0;
-}
-// Debug
-LUA_REGISTER_FUNC_DEF(lstg, ObjTable)
-{
-	return LPOOL.GetObjectTable(L);
-}
 // Deprecated
 LUA_REGISTER_FUNC_DEF(lstg, UpdateObjList)
 {
@@ -31,6 +19,10 @@ LUA_REGISTER_FUNC_DEF(lstg, UpdateSound)
 	return 0;
 }
 LUA_REGISTER_FUNC_DEF(lstg, Execute)
+{
+	return 0;
+}
+LUA_REGISTER_FUNC_DEF(lstg, ShowSplashWindow)
 {
 	return 0;
 }
