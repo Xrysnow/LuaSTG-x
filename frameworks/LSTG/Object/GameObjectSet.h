29845fe00dd1be906be677c5ed74fd663f872506
Xrysnow <xrysnow@outlook.com>
2020-10-14 10:31:08 +0800

    add GameObjectSet

---
+++ frameworks/LSTG/Object/GameObjectSet.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "GameObjectManager.h"
+
+namespace lstg
+{
+	/**
+	 * NOTE: This class is designed for lua.
+	 */
+	class GameObjectSet
+	{
+	public:
+		enum class SortMode
+		{
+			NONE,
+			DEFAULT,
+			LAYER
+		};
+	};
+}
