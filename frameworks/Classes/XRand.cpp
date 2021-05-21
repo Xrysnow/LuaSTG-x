f9802a47ef03386094fb8a5bbe457d53052c2be7
Xrysnow <xrysnow@outlook.com>
2021-05-21 15:43:41 +0800

    refactor Random

--- frameworks/Classes/XRand.cpp
+++ frameworks/Classes/XRand.cpp
@@ -1,64 +1,226 @@
 ﻿#include "XRand.h"
 
+#define SWITCH_TYPE(T, DIST)\
+	switch (T) {\
+	case GeneratorType::well512: return DIST(well512);\
+	case GeneratorType::minstd_rand0: return DIST(minstd_rand0);\
+	case GeneratorType::minstd_rand: return DIST(minstd_rand);\
+	case GeneratorType::mt19937: return DIST(mt19937);\
+	case GeneratorType::mt19937_64: return DIST(mt19937_64);\
+	case GeneratorType::ranlux24_base: return DIST(ranlux24_base);\
+	case GeneratorType::ranlux48_base: return DIST(ranlux48_base);\
+	case GeneratorType::ranlux24: return DIST(ranlux24);\
+	case GeneratorType::ranlux48: return DIST(ranlux48);\
+	case GeneratorType::knuth_b: return DIST(knuth_b);\
+	default:; }
+
 using namespace lstg;
 
-Random* Random::create()
+uint32_t Random::getDeviceSeed()
 {
-	auto ret = new (std::nothrow) Random();
-	if (ret)
-	{
-		return ret;
-	}
-	CC_SAFE_DELETE(ret);
-	return nullptr;
-}
-
-Random* Random::create(uint32_t seed)
-{
-	auto ret = new (std::nothrow) Random();
-	if (ret)
-	{
-		ret->setSeed(seed);
-		return ret;
-	}
-	CC_SAFE_DELETE(ret);
-	return nullptr;
+	static std::random_device device;
+	return device();
 }
 
 void Random::setSeed(uint32_t seed)
 {
-	random.setSeed(seed);
+	switch (_type)
+	{
+	case GeneratorType::well512: well512.seed(seed); break;
+	case GeneratorType::minstd_rand0: minstd_rand0.seed(seed); break;
+	case GeneratorType::minstd_rand: minstd_rand.seed(seed); break;
+	case GeneratorType::mt19937: mt19937.seed(seed); break;
+	case GeneratorType::mt19937_64: mt19937_64.seed(seed); break;
+	case GeneratorType::ranlux24_base: ranlux24_base.seed(seed); break;
+	case GeneratorType::ranlux48_base: ranlux48_base.seed(seed); break;
+	case GeneratorType::ranlux24: ranlux24.seed(seed); break;
+	case GeneratorType::ranlux48: ranlux48.seed(seed); break;
+	case GeneratorType::knuth_b: knuth_b.seed(seed); break;
+	default:;
+	}
+	_seed = seed;
 }
 
 uint32_t Random::getSeed()
 {
-	return random.getSeed();
+	return _seed;
 }
 
 int32_t Random::randInt(int32_t a, int32_t b)
 {
-	return a + int32_t(random.getRandUInt(uint32_t(b - a)));
+	if (_type == GeneratorType::well512)
+		return a + int32_t(well512.getRandUInt(uint32_t(b - a)));
+	if (a > b)
+		std::swap(a, b);
+	const std::uniform_int_distribution<int32_t> uniform_int(a, b);
+	SWITCH_TYPE(_type, uniform_int);
+	return 0;
 }
 
 float Random::randFloat(float a, float b)
 {
-	return random.getRandFloat(a, b);
+	double v = 0;
+	switch (_type)
+	{
+	case GeneratorType::well512: return well512.getRandFloat(a, b);
+	case GeneratorType::minstd_rand0: v = uniform_real1(minstd_rand0); break;
+	case GeneratorType::minstd_rand: v = uniform_real1(minstd_rand); break;
+	case GeneratorType::mt19937: v = uniform_real1(mt19937); break;
+	case GeneratorType::mt19937_64: v = uniform_real1(mt19937_64); break;
+	case GeneratorType::ranlux24_base: v = uniform_real1(ranlux24_base); break;
+	case GeneratorType::ranlux48_base: v = uniform_real1(ranlux48_base); break;
+	case GeneratorType::ranlux24: v = uniform_real1(ranlux24); break;
+	case GeneratorType::ranlux48: v = uniform_real1(ranlux48); break;
+	case GeneratorType::knuth_b: v = uniform_real1(knuth_b); break;
+	default:;
+	}
+	return v * (b - a) + a;
 }
 
 int32_t Random::randSign()
 {
-	return random.getRandUInt(1) * 2 - 1;
+	if (_type == GeneratorType::well512)
+		return well512.getRandUInt(1) * 2 - 1;
+	return randInt(0, 1) * 2 - 1;
+}
+
+int64_t Random::uniform_int(int64_t a, int64_t b)
+{
+	const std::uniform_int_distribution<int64_t> dist(a, b);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+bool Random::bernoulli(double p)
+{
+	const std::bernoulli_distribution dist(p);
+	SWITCH_TYPE(_type, dist);
+	return false;
+}
+
+int32_t Random::binomial(int32_t t, double p)
+{
+	const std::binomial_distribution<int32_t> dist(t, p);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+int32_t Random::negative_binomial(int32_t k, double p)
+{
+	const std::negative_binomial_distribution<int32_t> dist(k, p);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+int32_t Random::geometric(double p)
+{
+	const std::geometric_distribution<int32_t> dist(p);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+int32_t Random::poisson(double mean)
+{
+	const std::poisson_distribution<int32_t> dist(mean);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::extreme_value(double a, double b)
+{
+	const std::extreme_value_distribution<double> dist(a, b);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::chi_squared(double n)
+{
+	const std::chi_squared_distribution<double> dist(n);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::cauchy(double a, double b)
+{
+	const std::cauchy_distribution<double> dist(a, b);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::fisher_f(double m, double n)
+{
+	const std::fisher_f_distribution<double> dist(m, n);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::student_t(double n)
+{
+	const std::student_t_distribution<double> dist(n);
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+int32_t Random::discrete(const std::vector<double>& weights)
+{
+	const std::discrete_distribution<int32_t> dist(weights.begin(), weights.end());
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::piecewise_constant(const std::vector<double>& sequence, const std::vector<double>& weights)
+{
+	if (sequence.size() != weights.size() + 1)
+		return 0;
+	const std::piecewise_constant_distribution<double> dist(sequence.begin(), sequence.end(), weights.begin());
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+double Random::piecewise_linear(const std::vector<double>& sequence, const std::vector<double>& weights)
+{
+	if (sequence.size() != weights.size())
+		return 0;
+	const std::piecewise_linear_distribution<double> dist(sequence.begin(), sequence.end(), weights.begin());
+	SWITCH_TYPE(_type, dist);
+	return 0;
+}
+
+Random::Random(uint32_t seed, GeneratorType type)
+	: _type(type), uniform_real(0.0, 1.0), uniform_real1(0.0, std::nextafter(1.0, 2.0))
+{
+	switch (_type)
+	{
+	case GeneratorType::well512: well512 = {}; break;
+	case GeneratorType::minstd_rand0: minstd_rand0 = {}; break;
+	case GeneratorType::minstd_rand: minstd_rand = {}; break;
+	case GeneratorType::mt19937: mt19937 = {}; break;
+	case GeneratorType::mt19937_64: mt19937_64 = {}; break;
+	case GeneratorType::ranlux24_base: ranlux24_base = {}; break;
+	case GeneratorType::ranlux48_base: ranlux48_base = {}; break;
+	case GeneratorType::ranlux24: ranlux24 = {}; break;
+	case GeneratorType::ranlux48: ranlux48 = {}; break;
+	case GeneratorType::knuth_b: knuth_b = {}; break;
+	default: ;
+	}
+	Random::setSeed(seed);
 }
 
 double Random::_rand()
 {
-	return random.getRandFloat();
-}
-
-Random::Random()
-{
-}
-
-Random::~Random()
-{
+	switch (_type)
+	{
+	case GeneratorType::well512: return uniform_real(well512);
+	case GeneratorType::minstd_rand0: return uniform_real(minstd_rand0);
+	case GeneratorType::minstd_rand: return uniform_real(minstd_rand);
+	case GeneratorType::mt19937: return uniform_real(mt19937);
+	case GeneratorType::mt19937_64: return uniform_real(mt19937_64);
+	case GeneratorType::ranlux24_base: return uniform_real(ranlux24_base);
+	case GeneratorType::ranlux48_base: return uniform_real(ranlux48_base);
+	case GeneratorType::ranlux24: return uniform_real(ranlux24);
+	case GeneratorType::ranlux48: return uniform_real(ranlux48);
+	case GeneratorType::knuth_b: return uniform_real(knuth_b);
+	default: ;
+	}
+	return 0.0;
 }
--- frameworks/Classes/XRand.h
+++ frameworks/Classes/XRand.h
@@ -5,13 +5,20 @@
 
 namespace lstg
 {
-	class Random : public cocos2d::Ref, public xmath::random::Random
+	class Random : public xmath::random::Random
 	{
-	private:
-		RandomWELL512 random;
 	public:
-		static Random* create();
-		static Random* create(uint32_t seed);
+		enum class GeneratorType
+		{
+			well512,
+			minstd_rand0, minstd_rand,
+			mt19937, mt19937_64,
+			ranlux24_base, ranlux48_base,
+			ranlux24, ranlux48,
+			knuth_b
+		};
+
+		static uint32_t getDeviceSeed();
 
 		void setSeed(uint32_t seed) override;
 		uint32_t getSeed() override;
@@ -23,12 +30,65 @@ namespace lstg
 		/** get random 1 or -1 */
 		int32_t randSign();
 
-	private:
-		double _rand() override;
-		Random();
-	public:
-		virtual ~Random();
+		int64_t uniform_int(int64_t a, int64_t b);
+		/** produces bool values on a Bernoulli distribution. */
+		bool bernoulli(double p);
+		/** produces integer values on a binomial distribution. */
+		int32_t binomial(int32_t t, double p);
+		/** produces integer values on a negative binomial distribution. */
+		int32_t negative_binomial(int32_t k, double p);
+		/** produces integer values on a geometric distribution. */
+		int32_t geometric(double p);
+
+		/** produces integer values on a poisson distribution. */
+		int32_t poisson(double mean);
+		/** produces real values on an extreme value distribution. */
+		double extreme_value(double a, double b);
+
+		/** produces real values on a chi-squared distribution. */
+		double chi_squared(double n);
+		/** produces real values on a Cauchy distribution. */
+		double cauchy(double a, double b);
+		/** produces real values on a Fisher's F-distribution. */
+		double fisher_f(double m, double n);
+		/** produces real values on a Student's t-distribution. */
+		double student_t(double n);
+
+		/** produces random integers on a discrete distribution. */
+		int32_t discrete(const std::vector<double>& weights);
+		/** produces real values distributed on constant subintervals. */
+		double piecewise_constant(
+			const std::vector<double>& sequence,
+			const std::vector<double>& weights);
+		/** produces real values distributed on defined subintervals. */
+		double piecewise_linear(
+			const std::vector<double>& sequence,
+			const std::vector<double>& weights);
+
+		Random(uint32_t seed = 0, GeneratorType type = GeneratorType::well512);
 		Random(const Random &) = delete;
 		Random &operator =(const Random &) = delete;
+
+	protected:
+		double _rand() override;
+
+		union
+		{
+			RandomWELL512 well512;
+			std::minstd_rand0 minstd_rand0;
+			std::minstd_rand minstd_rand;
+			std::mt19937 mt19937;
+			std::mt19937_64 mt19937_64;
+			std::ranlux24_base ranlux24_base;
+			std::ranlux48_base ranlux48_base;
+			std::ranlux24 ranlux24;
+			std::ranlux48 ranlux48;
+			std::knuth_b knuth_b;
+		};
+		GeneratorType _type;
+		uint32_t _seed = 0;
+		std::uniform_real_distribution<double> uniform_real;
+		std::uniform_real_distribution<double> uniform_real1;
+
 	};
 }
